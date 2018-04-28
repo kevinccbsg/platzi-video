135b6203af35f02503ab31fb44bc7074f2536352
kevinccbsg <kevinccbsg@gmail.com>
2018-04-28 00:04:26 -0400

    Añadidos test de renderizado a el componente playlist

---
+++ src/playlist/components/playlist.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Playlist from './playlist';
+import { playlist as playlistFixture } from '../../fixtures';
+
+const props = {
+  playlist: playlistFixture,
+};
+
+describe('Playlist', () => {
+  const playlist = shallow(<Playlist {...props} />);
+  it('renders div with class Playlist', () => {
+    expect(playlist.find('div.Playlist').exists()).toBe(true);
+  });
+
+  it('renders two Media componenet', () => {
+    expect(playlist.find('Media')).toHaveLength(2);
+  });
+});
