c8f37ef0a80f68b1c3cdab08da1dae10b443d79f
kevinccbsg <kevinccbsg@gmail.com>
2018-04-28 00:32:52 -0400

    Añadidos test al componente visual de error

--- src/error/components/regular-error.js
+++ src/error/components/regular-error.js
@@ -1,8 +1,9 @@
 import React from 'react';
-function RegularError(props) {
+
+const RegularError = (props) => {
   return (
     <h1 style={{color: 'white'}}>Ha ocurrido un error</h1>
   )
-}
+};
 
 export default RegularError;
