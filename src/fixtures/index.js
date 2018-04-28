03543c3a11035c634e500c134c82d4b12d085d7c
kevinccbsg <kevinccbsg@gmail.com>
2018-04-28 00:03:36 -0400

    Añadidos fixtures a la aplicación para tener datos constantes para los tests

---
+++ src/fixtures/index.js
@@ -0,0 +1,13 @@
+
+export const media = {
+  title: '¿Qué es responsive Design?',
+  author: 'LeonidasEsteban',
+  type: 'video',
+  cover: './images/covers/responsive.jpg',
+  src: 'http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4',
+};
+
+export const playlist = [
+  { ...media, id: 0 },
+  { ...media, id: 1 },
+];
